5e6e8ae86746bc8c88cebe7f94e41abec7166f44
Harold Kim <harold.kim@ucalgary.ca>
2025-03-23 16:25:58 -0600

    added more files

--- my-lms-app/src/components/Header.js
+++ my-lms-app/src/components/Header.js
@@ -3,8 +3,9 @@ import React from 'react';
 function Header() {
     return (
         <div>
-            <div><img src = './images/logo.jpg' alt = "LMS logo" width = "10%" /></div>
-            <div><h1>LMS - Learning Management System</h1></div>
+            <div>
+                <img src = './images/logo.jpg' alt = "LMS logo" width = "10%" />
+                <h1>LMS - Learning Management System</h1></div>
             <div class = "navigation">
                 <a href = "Homepage.js">Homepage</a>
                 <a href = "CoursesPage.js">Courses</a>
--- my-lms-app/src/components/MainSection.js
+++ my-lms-app/src/components/MainSection.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
 function MainSection() {
+    
     return (
         <div>
             <h2>About LMS</h2>
             <p>The Learning Management System (LMS) helps students and instructors manage courses, quizzes, and track performance efficiently.</p>
-            </div>
+            <h2>Featured Courses</h2>
+
+            <h2>Testimonials</h2>
+        </div>
     );
 }
 
