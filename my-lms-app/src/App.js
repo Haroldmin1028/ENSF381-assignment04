6a6e0c01497d67c3388bb8900e969c789427c690
Harold Kim <harold.kim@ucalgary.ca>
2025-03-29 14:40:36 -0600

    fixed links, actually finished Homepage, Header and Footer also finished

--- my-lms-app/src/App.js
+++ my-lms-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Homepage from './Homepage.js';
 import CoursesPage from './CoursesPage.js';
@@ -7,11 +6,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-      <div>
-
-
-        <Homepage />
-      </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/courses" element={<CoursesPage/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- my-lms-app/src/components/Header.js
+++ my-lms-app/src/components/Header.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import logo from '../images/logo.jpg';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-import Homepage from '../Homepage.js';
-import CoursesPage from '../CoursesPage.js';
-import Login from '../Login.js';
 
 function Header() {
     return (
         <div>
-
             <header>
                 <img src = {logo} alt = "LMS logo" width = "10%"></img>
                 <h1>LMS - Learning Management System</h1>
             </header>
-{/*             <div class = "navigation">
-                <Link to = "/">Homepage</Link>
-                <Link to = "/courses">Courses</Link>
-                <Link to = "/login">Login</Link>
-            </div> */}
             <div class = "navigation">
                 <a href = "/">Homepage</a>
                 <a href = "/courses">Courses</a>
                 <a href = "/login">Login</a>
             </div>
-
-
         </div>
     );
 }
